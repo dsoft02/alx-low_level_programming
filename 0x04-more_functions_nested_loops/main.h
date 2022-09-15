b60c9ba242b4276d2454cbeed8c27195d12c37b2
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-15 01:47:24 -0700

    Task 0 - function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * _islower - prints if c is lower case
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _islower(int c);
+
+#endif
