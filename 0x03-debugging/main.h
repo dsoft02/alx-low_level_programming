c9acb35070bb601e4888eed4929ad7d347b45f8c
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-14 17:17:02 -0700

    Task 2 - Fix the code in 2-largest_number.c so that it correctly prints out the largest of three numbers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - function to test if a number is positive or negative
+ * @i: parameter to to test
+ */
 void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c);
 
 #endif
