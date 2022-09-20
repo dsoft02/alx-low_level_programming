704654fce22b35d7e3f0576a6453ad07be0f13f8
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-19 18:48:44 -0700

    Task 0 - function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - update a pointer's referenced value to 98
+ *
+ * @n: the number to be updated
+ *
+ */
+void reset_to_98(int *n);
+
+#endif
