468c4239940b9a35f684dfdfa2519cd25f810095
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-21 21:17:50 -0700

    Task 4 - function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -45,6 +45,15 @@ char *_strncpy(char *dest, char *src, int n);
  */
 int _strcmp(char *s1, char *s2);
 
+/**
+ * reverse_array - function that reverses
+ * the content of an array of integers
+ *
+ * @a: array to reverse
+ * @n: size of array
+ */
+void reverse_array(int *a, int n);
+
 
 
 
