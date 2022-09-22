7720423cb848bb037dc23986aeb398b57642f555
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-21 20:57:24 -0700

    Task 0 - function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src);
+
+
+
+
+#endif
