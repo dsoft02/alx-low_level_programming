a3b1354a739df368291675f6f4e1214d17651dc2
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-26 12:46:21 -0700

    Task 0 - function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: number of times to fill
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
