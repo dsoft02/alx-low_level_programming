3effcf81e953889ac9758416e0abaa93dd1c5aa0
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-28 01:07:18 -0700

    Task 1 - function that prints a string in reverse.

--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,7 @@
 ## 0x08. C - Recursion
 
+[0-puts_recursion.c](./0-puts_recursion.c) - function that prints a string in reverse.
+
+[1-print_rev_recursion](./1-print_rev_recursion) - function that prints a string in reverse.
+
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,11 @@ int _putchar(char c);
  */
 void _puts_recursion(char *s);
 
+/**
+ * _print_rev_recursion - funtion that prints a string in reverse
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s);
+
 
 #endif
