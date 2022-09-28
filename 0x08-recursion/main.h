8e6d21990e8912df467d1face03252957b2aeccb
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-28 01:00:03 -0700

    Task 0 - function that prints a string, followed by a new line.

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - funtion that prints a string
+ * @s: string to print
+ */
+void _puts_recursion(char *s);
+
+
+#endif
