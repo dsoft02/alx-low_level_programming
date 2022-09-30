33114d0dfb815e7bc4cfc35ff881ef7e6cc12a7b
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-09-30 09:13:20 -0700

    Task 0 - program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+## 0x0A. C - argc, argv
+
+[0-whatsmyname.c](./0-whatsmyname.c) - program that prints its name, followed by a new line.
+
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * main - main function
+ * @argc: argument counts
+ * @argv: array of arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[]);
+
+#endif
